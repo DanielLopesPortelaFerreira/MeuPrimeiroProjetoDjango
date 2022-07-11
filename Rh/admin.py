fe6c7f9974b55d402049436b1f379247bbbeb735
Daniel Portela <androidanny274@gmail.com>
2022-07-11 20:33:47 -0300

    Versão Inicial do Projeto

---
+++ Rh/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from Rh.models import Departamento, Funcionario
+
+# Register your models here.
+admin.site.register(Funcionario)
+admin.site.register(Departamento)
